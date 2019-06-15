d9071fe3638b5a53aa9d908a13d380682a02e7d6
Vladislav Ovchinnikov <ovchinnikovlad@gmail.com>
2019-06-15 13:05:22 +0300

    Test-project Kitter

---
+++ src/main/java/com.example.kitter/repos/UserRepo.java
@@ -0,0 +1,10 @@
+package com.example.kitter.repos;
+
+import com.example.kitter.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+
+    User findByActivationCode(String code);
+}
